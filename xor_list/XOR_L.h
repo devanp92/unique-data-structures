3736dd36680d7cfc23b52aef4d0caf8952b54358
Devan Patel <devanppatel92@gmail.com>
2015-01-21 19:17:27 -0500

    Adding xor list

---
+++ xor_list/XOR_L.h
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  XOR_L.h
+ *
+ *    Description: "An XOR linked list is a data structure used in computer programming. It takes advantage of the bitwise XOR operation to decrease storage requirements for doubly linked lists." https://en.wikipedia.org/wiki/XOR_linked_list 
+ *
+ *        Version:  1.0
+ *        Created:  01/15/2015 15:17:09
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Devan Patel (), devanp92@ufl.edu
+ *
+ * =====================================================================================
+ */
+
+#ifndef __XOR_L_H__
+#define __XOR_L_H__
+#include "Node.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+#endif
